16c1f2a175840f43cdd7203ef893b81c1e5931c2
Jaedo <jaime.aedosanhueza@gmail.com>
2014-09-12 16:58:00 -0400

    add 5 activity

---
+++ primer_sitio/PrimerSitio/Default4.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default4.aspx.cs" Inherits="Default4" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 437px;
+        }
+        .auto-style2 {
+            width: 167px;
+        }
+        .auto-style3 {
+            width: 213px;
+        }
+        .auto-style4 {
+            width: 209px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Label ID="lbl_mensaje" runat="server"></asp:Label>
+
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="lbl_edad" runat="server" Text=""></asp:Label>
+                </td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="txt_edad" runat="server" Width="119px"></asp:TextBox>
+                </td>
+                <td class="auto-style4">
+                    <asp:RadioButton ID="radiobutton_year" runat="server" Text="Años" GroupName="tipo" />
+                    <asp:RadioButton ID="radiobutton_month" runat="server" Text="Meses" GroupName="tipo" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="lbl_nombre" Text="" runat="server" />
+                </td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="txt_nombre" runat="server" />
+                </td>
+                <td class="auto-style4">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lbl_peso" Text="" runat="server" />
+                </td>
+                <td class="auto-style3">
+                    
+                    <asp:TextBox ID="txt_peso" runat="server"></asp:TextBox>
+                    
+                </td>
+                <td class="auto-style4">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td class="auto-style3">
+                    <asp:Button ID="btn_calcular" runat="server" Text="Calcular" OnClick="btn_calcular_Click" />
+                </td>
+            </tr>
+        </table>
+    </div>
+        <asp:Label ID="lbl_mensaje2" Text="" runat="server" />
+    </form>
+</body>
+</html>
---
+++ primer_sitio/PrimerSitio/Default4.aspx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default4 : System.Web.UI.Page
+{
+    double pesoIdeal;
+    bool no_es;
+
+    Boolean tipo_edad;
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        MensajeInicial();
+        
+    }
+    Double calcularPesoIdeal(double edad)
+    {
+        if (!unidad_edad() && edad >= 3 || !unidad_edad() && edad <= 12)
+        {
+            pesoIdeal = (edad + 9) / 2;
+        }
+
+        if (unidad_edad() && edad >= 2 || unidad_edad() && edad <= 6)
+        {
+            pesoIdeal = (edad * 2) + 8.5;
+        }
+        if (unidad_edad() && edad >= 7 || unidad_edad() && edad <= 15)
+        {
+            pesoIdeal = (edad * 3) + 3;
+        }
+            return (pesoIdeal);
+    }
+
+    void MostrarPesoIdeal(double pesoideal)
+    {
+        double peso_actual = Convert.ToDouble(txt_peso.Text);
+
+        if (unidad_edad())
+        {
+            if (peso_actual > pesoideal)
+            {
+                lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text + " es " + pesoideal + " Kilos. Está con sobrepeso";
+            }
+            if (peso_actual < pesoideal)
+            {
+                lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text + " es " + pesoideal + " Kilos. Está bajo peso";
+            }
+            if (peso_actual == pesoideal)
+            {
+                lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text + " es " + pesoideal + " Kilos. Está con peso ideal";
+            }
+            
+        }
+
+        if (!unidad_edad())
+        {
+
+            if (peso_actual > pesoideal)
+            {
+                lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text + " es " + pesoideal + " gramos. Está con sobrepeso";
+            }
+            if (peso_actual < pesoideal)
+            {
+                lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text + " es " + pesoideal + " gramos. Está bajo peso";
+            }
+            if (peso_actual == pesoideal)
+            {
+                lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text + " es " + pesoideal + " gramos. Está con peso ideal";
+            }
+        }
+
+    }
+
+    void MensajeInicial()
+    {
+        lbl_mensaje.Text = "Peso Ideal de un niño/a: ";
+        lbl_edad.Text = "Ingrese la edad: ";
+        lbl_nombre.Text = "Ingrese el nombre: ";
+        lbl_peso.Text = "Ingrese el peso actual del niño/a: ";
+    }
+
+
+    String CapturaEdad()
+    {
+        string edad;
+        edad = txt_edad.Text;
+        return edad;
+    }
+
+    protected void btn_calcular_Click(object sender, EventArgs e)
+    {
+
+
+        if (PermiteEdad())
+        {
+            MostrarPesoIdeal(calcularPesoIdeal(Convert.ToDouble(CapturaEdad())));
+
+        }
+        else {
+            if (!PermiteEdad())
+            {
+                msj_close();
+
+            }
+        }
+    }
+
+    Boolean PermiteEdad()
+    {
+        int edad_ingresada = Convert.ToInt32(CapturaEdad());
+        if (unidad_edad())
+        {           
+            if (edad_ingresada > 15 || edad_ingresada < 2)
+            {
+                no_es = false;
+            }
+        }
+        if (!unidad_edad())
+        {
+            if (edad_ingresada > 12 || edad_ingresada < 3)
+            {
+                no_es = false;
+            }
+        }
+        return no_es;
+    }
+
+    void msj_close()
+    {
+        lbl_mensaje2.Text = "Edad incorrecta para realizar el calculo";
+    }
+    Boolean unidad_edad() {
+        if (radiobutton_month.Checked)
+        {
+            tipo_edad = false;
+        }
+        else {
+            if (radiobutton_year.Checked)
+            {
+                tipo_edad = true;
+            }
+        }
+        return tipo_edad;
+    }
+}
+
