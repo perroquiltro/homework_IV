9d50c8a4b21fbf55f78b720313a57ef36bb1ccd9
Jaedo <jaime.aedosanhueza@gmail.com>
2014-09-12 12:59:28 -0400

    add act. 3

---
+++ primer_sitio/PrimerSitio/Default3.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default3.aspx.cs" Inherits="Default3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lbl_mensaje" runat="server"></asp:Label>
+
+        <br />
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="lbl_edad" runat="server" Text=""></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txt_edad" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="lbl_nombre" Text="" runat="server" />
+                </td>
+                <td>
+                    <asp:TextBox ID="txt_nombre" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lbl_peso" Text="" runat="server" />
+                </td>
+                <td>
+                    
+                    <asp:TextBox ID="txt_peso" runat="server"></asp:TextBox>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                    <asp:Button ID="btn_calcular" runat="server" Text="Calcular" OnClick="btn_calcular_Click" />
+                </td>
+            </tr>
+        </table>
+    </div>
+        <asp:Label ID="lbl_mensaje2" Text="" runat="server" />
+    
+    </form>
+</body>
+</html>
---
+++ primer_sitio/PrimerSitio/Default3.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        MensajeInicial();
+    }
+    int calcularPesoIdeal(int edad)
+    {
+        int pesoIdeal = edad * 2 + 8;
+        return pesoIdeal;
+    }
+
+    void MostrarPesoIdeal(int pesoideal)
+    {
+        int peso_actual = Convert.ToInt32(txt_peso.Text);
+
+        if (peso_actual > pesoideal)
+        {
+            lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text +" es "+ pesoideal + " kilos. Está con sobrepeso";
+        }
+        if (peso_actual < pesoideal)
+        {
+            lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text +" es "+ pesoideal + " kilos. Está bajo peso";
+        }
+        if (peso_actual == pesoideal)
+        {
+            lbl_mensaje2.Text = "El peso ideal de " + txt_nombre.Text + " es " + pesoideal + " kilos. Está con peso ideal";
+        }
+
+    }
+
+    void MensajeInicial()
+    {
+        lbl_mensaje.Text = "Peso Ideal de un niño/a: ";
+        lbl_edad.Text = "Ingrese la edad: ";
+        lbl_nombre.Text = "Ingrese el nombre: ";
+        lbl_peso.Text = "Ingrese el peso actual del niño/a: ";
+    }
+
+
+    String CapturaEdad()
+    {
+        string edad;
+        edad = txt_edad.Text;
+        return edad;
+    }
+
+    protected void btn_calcular_Click(object sender, EventArgs e)
+
+    {
+        
+            if (PermiteEdad())
+            {
+                msj_close();
+            }
+            else
+            {
+                MostrarPesoIdeal(calcularPesoIdeal(Convert.ToInt16(CapturaEdad())));
+            }   
+       
+        
+    }
+
+    Boolean PermiteEdad()
+    {
+        int edad_ingresada = Convert.ToInt32(CapturaEdad());
+        
+
+        bool no_es = false;
+        if (edad_ingresada > 10 || edad_ingresada < 1)
+            no_es = true;
+        return no_es;
+    }
+
+    void msj_close() {
+        lbl_mensaje2.Text = "Edad incorrecta para realizar el calculo";
+    }
+}
